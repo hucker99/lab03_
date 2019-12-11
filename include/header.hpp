d6a791ae45cc907da995bdb793d210d28881456f
hucker99 <54907176+hucker99@users.noreply.github.com>
2019-12-11 21:49:01 +0300

    Update header.hpp

--- include/header.hpp
+++ include/header.hpp
@@ -2,5 +2,37 @@
 
 #ifndef INCLUDE_HEADER_HPP_
 #define INCLUDE_HEADER_HPP_
+//
+// Created by od341 on 25.09.2019.
+//
+#include <iostream>
+#include <string>
+#include <atomic>
+#include <map>
+
+template <typename T>
+class SharedPtr {
+public:
+    SharedPtr();
+    explicit SharedPtr(T* ptr);
+    explicit SharedPtr(const SharedPtr& r);
+    explicit SharedPtr(SharedPtr&& r);
+    ~SharedPtr();
+    auto operator=(const SharedPtr& r) -> SharedPtr&;
+    auto operator=(SharedPtr&& r) -> SharedPtr&;
+
+    operator bool() const;
+    auto operator*() const -> T&;
+    auto operator->() const -> T*;
+
+    auto get() -> T*;
+    void reset();
+    void reset(T* ptr);
+    void Pswap(SharedPtr& r);
+    auto use_count() const -> size_t;
+    static std::map<int64_t, std::atomic_uint >* ptr_table;
+private:
+    T* _ptr;
+};
 
 #endif // INCLUDE_HEADER_HPP_
